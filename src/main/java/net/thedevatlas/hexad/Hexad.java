4bdeb03905634c5276e67ddc935cfb664ffefb73
Jacob McGowan <73966737+TheDevAtlas@users.noreply.github.com>
2024-04-16 20:04:43 -0400

    Init

---
+++ src/main/java/net/thedevatlas/hexad/Hexad.java
@@ -0,0 +1,16 @@
+package net.thedevatlas.hexad;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Hexad implements ModInitializer {
+	public static final String MOD_ID = "hexad";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("The Dev Atlas - Init Hexad Program!");
+	}
+}
---
+++ src/main/java/net/thedevatlas/hexad/HexadClient.java
@@ -0,0 +1,11 @@
+package net.thedevatlas.hexad;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HexadClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient()
+    {
+
+    }
+}
