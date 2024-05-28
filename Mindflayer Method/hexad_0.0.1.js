175f755b53e08783fb25bc33882be378153af87d
Jacob McGowan <73966737+TheDevAtlas@users.noreply.github.com>
2024-05-27 21:29:10 -0400

    Control bots

--- Mindflayer Method/hexad_0.0.1.js
+++ Mindflayer Method/hexad_0.0.1.js
@@ -1,8 +1,8 @@
 const mineflayer = require('mineflayer');
-const mineflayerViewer = require('prismarine-viewer').mineflayer
+//const mineflayerViewer = require('prismarine-viewer').mineflayer
 
 const bots = [];
-const connectionInterval = 2000; // Delay in milliseconds between bot connections
+const connectionInterval = 100; // Delay in milliseconds between bot connections
 
 // Function to create a bot
 function createBot(index) {
@@ -14,32 +14,17 @@ function createBot(index) {
   });
 
   bot.once('spawn', () => {
-    // const x = (index % gridSize) * gridSpacing;
-    // const z = Math.floor(index / gridSize) * gridSpacing;
-    // bot.chat(`Moving to position ${x}, ${z}`);
-    // // Assuming flat terrain and ignoring y for simplicity
-    // bot.entity.position.x = 0;
-    // bot.entity.position.z = 0;
     bot.chat(`I have joined`);
     bot.attack(bot.nearestEntity);
   });
 
-  // bot.on('chat', (username, message) => {
-  //   if (username === bot.username) return;
-  //   bot.chat(message);
-  // });
-
-  // bot.on('kicked', (reason) => console.log(`Bot${index} was kicked: ${reason}`));
-  // bot.on('error', console.log);
-
-  // bots.push(bot);
+  bots.push(bot);
 
   // bot.once('spawn', () => {
   //   mineflayerViewer(bot, { port: 3007+index, firstPerson: false })
   // })
 }
 
-// Create 25 bots with a delay
 for (let i = 0; i < 25; i++) {
   setTimeout(() => {
     createBot(i);
---
+++ Mindflayer Method/hexad_0.1.1.js
@@ -0,0 +1,86 @@
+// Control bot individual by using its id // - collect bot 2 oak_log
+// OR
+// Control all bots using general command // - collect oak_log
+
+const mineflayer = require('mineflayer')
+const pathfinder = require('mineflayer-pathfinder').pathfinder
+const collectBlock = require('mineflayer-collectblock').plugin
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+class CollectorBot {
+  constructor(id) {
+    this.bot = mineflayer.createBot({
+      host: 'localhost',
+      port: 25565,
+      username: 'collector'+id,
+    })
+
+    this.id = id;
+
+    this.bot.loadPlugin(pathfinder)
+    this.bot.loadPlugin(collectBlock)
+
+    this.bot.on('chat', (username, message) => {
+      const args = message.split(' ')
+      if (args[0] !== 'collect') return
+
+      if (args[1] == 'bot')
+      {
+        if(args[2] == id)
+        {
+          const blockType = this.bot.registry.blocksByName[args[3]]
+          if (!blockType) {
+            this.bot.chat("I don't know any blocks with that name.")
+            return
+          }
+  
+          this.bot.chat('Collecting the nearest ' + blockType.name)
+          this.processLoop(blockType, this.id)
+        }
+      }
+      else
+      {
+        const blockType = this.bot.registry.blocksByName[args[1]]
+        if (!blockType) {
+          this.bot.chat("I don't know any blocks with that name.")
+          return
+        }
+
+        this.bot.chat('Collecting the nearest ' + blockType.name)
+        this.processLoop(blockType, this.id)
+      }
+
+      
+    })
+
+    this.bot.on('kicked', console.log)
+    this.bot.on('error', console.log)
+  }
+
+  async processLoop(blockType, id) {
+    while (true) {
+      await delay(500 * id);
+
+      const block = this.bot.findBlock({
+        matching: blockType.id,
+        maxDistance: 64
+      })
+
+      if (!block) {
+        this.bot.chat("I don't see that block nearby.")
+        return
+      }
+
+      if(this.bot.pathfinder.goal.isEnd)
+
+      this.bot.collectBlock.collect(block);
+      await delay(2000);
+    }
+  }
+}
+
+// Spawning multiple bots
+const botCount = 5; // Number of bots you want to spawn
+for (let i = 1; i <= botCount; i++) {
+  new CollectorBot(i);
+}
